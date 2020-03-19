a96e8259d991fddad830f61a87e6daba11a57ec4
rshtishi <rando.shtishi@@gmail.com>
2020-03-19 21:53:59 +0100

    Adding department service

---
+++ department/src/main/java/com/github/rshtishi/departme/DepartmentApplication.java
@@ -0,0 +1,13 @@
+package com.github.rshtishi.departme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DepartmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DepartmentApplication.class, args);
+	}
+
+}
