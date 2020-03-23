7c54c28c06e6b8ff50cc786a5712da64c87aa145
rshtishi <rando.shtishi@@gmail.com>
2020-03-23 15:03:34 +0100

    minor fix

--- department/src/main/java/com/github/rshtishi/department/entity/Department.java
+++ department/src/main/java/com/github/rshtishi/department/entity/Department.java
@@ -16,6 +16,6 @@ public class Department {
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private int id;
 	private String name;
-	private String noOfEmployees;
+	private int noOfEmployees;
 
 }
