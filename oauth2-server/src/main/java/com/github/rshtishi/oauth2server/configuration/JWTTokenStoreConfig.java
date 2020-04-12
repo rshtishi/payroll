0637122677ac3499ab8898ca1702e1055fad3b0d
rshtishi <rando.shtishi@@gmail.com>
2020-04-12 21:50:55 +0200

    modifying the authentication service to issue jwt token

---
+++ oauth2-server/src/main/java/com/github/rshtishi/oauth2server/configuration/JWTTokenStoreConfig.java
@@ -0,0 +1,36 @@
+package com.github.rshtishi.oauth2server.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+@Configuration
+public class JWTTokenStoreConfig {
+	
+	@Bean
+	public JwtAccessTokenConverter jwtAccessTokenConverter() {
+		JwtAccessTokenConverter jwtAccessTokenConverter = new JwtAccessTokenConverter();
+		jwtAccessTokenConverter.setSigningKey("345345fsdgsf5345");
+		return jwtAccessTokenConverter;
+	}
+	
+	@Bean
+	public TokenStore tokenStore() {
+		return new JwtTokenStore(jwtAccessTokenConverter());
+	}
+	
+	@Bean
+	@Primary
+	public DefaultTokenServices defaultTokenServices() {
+		DefaultTokenServices defaultTokenServices = new DefaultTokenServices();
+		defaultTokenServices.setTokenStore(tokenStore());
+		defaultTokenServices.setSupportRefreshToken(true);
+		return defaultTokenServices;
+	}
+	
+	
+}
--- oauth2-server/src/main/java/com/github/rshtishi/oauth2server/configuration/OAuth2Config.java
+++ oauth2-server/src/main/java/com/github/rshtishi/oauth2server/configuration/OAuth2Config.java
@@ -1,5 +1,7 @@
 package com.github.rshtishi.oauth2server.configuration;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,6 +11,10 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
+import org.springframework.security.oauth2.provider.token.TokenEnhancerChain;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 
 @Configuration
 public class OAuth2Config extends AuthorizationServerConfigurerAdapter {
@@ -22,24 +28,30 @@ public class OAuth2Config extends AuthorizationServerConfigurerAdapter {
 	@Autowired
 	OAuth2ConfigParameters oauth2ConfigParameters;
 
+
+	@Autowired
+	private TokenStore tokenStore;
+	@Autowired
+	private JwtAccessTokenConverter jwtAccessTokenConverter;
+
 	@Bean
 	public OAuth2ConfigParameters oAuth2ConfigParameters() {
 		return new OAuth2ConfigParameters();
 	}
-	
-    @Override
-    public void configure(AuthorizationServerEndpointsConfigurer endpoints) {
-        endpoints
-                .authenticationManager(authenticationManager)
-                .userDetailsService(userDetailsService);
-    }
-	
+
+	@Override
+	public void configure(AuthorizationServerEndpointsConfigurer endpoints) {
+		TokenEnhancerChain tokenEnhancerChain = new TokenEnhancerChain();
+		tokenEnhancerChain.setTokenEnhancers(Arrays.asList(jwtAccessTokenConverter));
+		endpoints.tokenStore(tokenStore).accessTokenConverter(jwtAccessTokenConverter)
+				.authenticationManager(authenticationManager).userDetailsService(userDetailsService);
+	}
+
 	@Override
 	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-		clients.inMemory()
-		.withClient(oauth2ConfigParameters.getClient().getId())
-		.secret(passwordEncoder.encode(oauth2ConfigParameters.getClient().getSecret()))
-		.authorizedGrantTypes( "refresh_token","password","client_credentials")
-        .scopes("webclient","mobileclient");
+		clients.inMemory().withClient(oauth2ConfigParameters.getClient().getId())
+				.secret(passwordEncoder.encode(oauth2ConfigParameters.getClient().getSecret()))
+				.authorizedGrantTypes("refresh_token", "password", "client_credentials")
+				.scopes("webclient", "mobileclient");
 	}
 }
