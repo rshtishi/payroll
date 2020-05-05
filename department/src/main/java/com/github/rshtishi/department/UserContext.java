9601880fc79ec58c24717de791e412bbd421be33
rshtishi <rando.shtishi@@gmail.com>
2020-05-05 20:11:07 +0200

    log agregation with elk

--- department/src/main/java/com/github/rshtishi/department/UserContext.java
+++ department/src/main/java/com/github/rshtishi/department/UserContext.java
@@ -8,9 +8,7 @@ import lombok.Setter;
 public class UserContext {
 
 	public static final String AUTHORIZATION = "Authorization";
-	public static final String CORRELATION_ID = "correlation-id";
 
 	private String authorization = new String();
-	private String correlationId = new String();
 
 }
